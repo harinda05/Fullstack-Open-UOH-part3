ccf3c4b93596bf98805b32d105077c16a61f3d0a
harinda05 <harindaburndown@gmail.com>
2025-01-04 12:52:20 +0200

    cors fix

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ require('dotenv').config()
 const cors = require('cors')
 const Person = require('./models/person')
 
-app.use(cors());
 const app = express()
 app.use(express.json())
+app.use(cors());
 
 app.use(express.static('dist'))
 
